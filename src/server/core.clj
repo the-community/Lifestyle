2b09452375b58278503a05674c807e53724692af
eowbotm <eowbotm@gmail.com>
2015-03-06 22:44:49 -0500

    Http take 2
    Still doesn't work

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,8 +1,11 @@
 (ns server.core)
+(require 'ring.adapter.jetty)
+
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
+   
+   
 (defn -main [& args]
-	(use 'ring.adapter.jetty)
-	(run-jetty handler {:port 3000}))
+	ring.adapter.jetty/run-jetty handler {:port 3000})
