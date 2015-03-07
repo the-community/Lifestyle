71d30accd7f642477fe488c6096889ad119e8488
Chris Hairfield <chairfield@gmail.com>
2015-03-06 20:40:00 -0800

    We have a webpage
    but it downloads

--- src/server/handler.clj
+++ src/server/handler.clj
@@ -1,10 +1,11 @@
 (ns server.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+			[ring.util.response :refer [resource-response response]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
   (route/not-found "Not Found"))
 
 (def app
